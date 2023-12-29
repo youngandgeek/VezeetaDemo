25b2ac37ae724227d12124de90b028d67d968e99
youngandgeek <eng.emanessam@gmail.com>
2023-12-29 14:05:30 +0200

    -

---
+++ DomainLayer/IAdminRepository.cs
@@ -0,0 +1,12 @@
+﻿using DomainLayer.Models.DTO;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+namespace DomainLayer
+{
+  /**  public interface IAdminRepository
+    {
+       Task<IdentityResult> CreateDoctor(DoctorSignUpModel doctorSignUpModel);
+
+    }
+  **/
+}
